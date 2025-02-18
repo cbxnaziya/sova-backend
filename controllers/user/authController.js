d907645bd7dc3253093e7f0ba0907fb0ebfdf8f7
cbxnaziya <naziya@norbaq.net>
2025-02-18 18:42:04 +0530

    api updation

--- controllers/user/authController.js
+++ controllers/user/authController.js
@@ -32,20 +32,20 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
+
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Invalid Credentials" });
+    if (!user) return res.status(400).json({   success: false,message: "Invalid Credentials" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
+    if (!isMatch) return res.status(400).json({   success: false,message: "Invalid Credentials" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-    res.json({ token });
+    res.json({  success: true,    message: "Login successful", token });
+
   } catch (error) {
     res.status(500).json({ message: "Server Error" });
   }
 };
-
-
 
 
 exports.requestPasswordReset = async (req, res) => {
